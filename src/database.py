6bb4b96085f2b22f057e15cb31b4b2db96ed0255
Arfan Kafait <irfankafait@gmail.com>
2026-07-18 03:00:12 +0500

    feat: create DatabaseManager class sekeleton

---
+++ src/database.py
@@ -0,0 +1,13 @@
+import mysql.connector
+from mysql.connector import Error
+from config import * 
+from logger import logger
+
+
+class DatabaseManager:
+
+    """
+    Handles all database operations for the RoadSense project.
+    """
+    pass
+
